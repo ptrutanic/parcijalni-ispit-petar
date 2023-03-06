a7c055e59f6d067459b999dc5ec4124ffab1d6be
Luka Horvatic <lhorvatic@infobip.com>
2023-03-06 16:56:54 +0100

    Luka Horvatić: Parcijalni ispit

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 const router = express.Router();
 
-router.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
+// Load Chance
+var Chance = require("chance");
+
+// Instantiate Chance so it can be used
+var chance = new Chance();
+
+// Use Chance here.
+var my_random_string = chance.string();
+
+console.log(my_random_string);
+
+router.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/index.html"));
 });
 
-app.use('/assets', express.static(__dirname + '/assets'));
-app.use('/', router);
+app.use("/assets", express.static(__dirname + "/assets"));
+app.use("/", router);
 
 app.listen(process.env.port || 3000);
-console.log('Running at Port 3000');
+console.log("Running at Port 3000");
